7a8345093ee3c56e19f643d1e7377c9175ed4c10
Andres Garcia Larrosa <andriusmw@yahoo.es>
2024-01-25 10:56:14 +0100

    val & var

---
+++ src/main/kotlin/com/kotlinplayground/basics/VariablesFile.kt
@@ -0,0 +1,12 @@
+package com.kotlinplayground.basics
+
+fun main() {
+    // val = const
+    val name : String = "Dilip";
+    println(name)
+
+    var age : Int = 34
+    println(age)
+    age = 35
+    println(age)
+}
