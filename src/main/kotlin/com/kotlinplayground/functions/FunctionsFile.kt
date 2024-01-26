4bed35621623887f1b043a4ad9a079468d593203
Andres Garcia Larrosa <andriusmw@yahoo.es>
2024-01-26 11:21:16 +0100

    default value parameters

--- src/main/kotlin/com/kotlinplayground/functions/FunctionsFile.kt
+++ src/main/kotlin/com/kotlinplayground/functions/FunctionsFile.kt
@@ -1,17 +1,33 @@
 package com.kotlinplayground.functions
 
+import java.time.LocalDate
+
+//----------------------------------------------------
 // when a functions does not return anything it has a return type of : Unit
 fun printName(name: String){
 
     println("Name is: $name")
 }
+//´-----------------------------------------------------
 //------------VAR 1  , VAR 2 : RETURN TYPE OF THE VALUE
 fun addition(x: Int, y: Int): Int {
 
     return x+y
 }
-
+//-------------------------------------------------------------
 fun addition_approach1(x: Int, y: Int) = x+y
+
+
+//-------------------------------------------------
+// using = "default value" we can assign default value to function properties in case we do get them when
+//the function is called
+fun printPersonDetails(name: String,
+                       email: String = "",
+                       dob: LocalDate = LocalDate.now()){
+    println("Name is $name and the email is $email" +
+            "and the dob is $dob")
+}
+
 
 
 //-------------------------------------
@@ -22,5 +38,9 @@ fun main(){
 
     val result1 =  addition_approach1(1,2)
     println("Result is $result1")
+
+    printPersonDetails("Dilip", "abc@gmail.com", LocalDate.parse("2000-01-01"))
+    printPersonDetails("Dilip")
+
 }
 
