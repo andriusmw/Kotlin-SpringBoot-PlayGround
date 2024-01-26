febaf1ecebd64f13cf6e3b528b256a4897c84261
Andres Garcia Larrosa <andriusmw@yahoo.es>
2024-01-26 09:51:02 +0100

    while and dowhile

--- src/main/kotlin/com/kotlinplayground/basics/LoopsAndRangesFile.kt
+++ src/main/kotlin/com/kotlinplayground/basics/LoopsAndRangesFile.kt
@@ -1,7 +1,8 @@
 package com.kotlinplayground.basics
 
+//-----------------------MAIN ----------------------
 fun main() {
-
+/*
     val range = 1..10
     for(i in range){
         println("i : $i")
@@ -15,7 +16,29 @@ fun main() {
     //it goes 2 by 2
     for(i in reverseRange step 2){
         println("ReverseRange with Skip : $i")
+    } */
+
+ exploreWhile()
+    exploreDoWhile()
+
+}
+
+
+// -----------------------------END MAIN ----------------
+
+fun exploreDoWhile() {
+    // this enter the do action phase before it is checked
+  var i = 0
+    do{
+        println("Value of i is: $i")
+        i++
+    } while (i < 5)
+}
+
+fun   exploreWhile(){
+    var x = 1
+    while(x < 5){
+        println("Value of X is :$x")
+        x++
     }
-
-
 }
