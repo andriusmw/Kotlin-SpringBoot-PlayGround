521070b465bcc060784feb5becbc56fbfd96f555
Andres Garcia Larrosa <andriusmw@yahoo.es>
2024-01-26 09:42:49 +0100

    ranges with for loop

--- src/main/kotlin/com/kotlinplayground/basics/LoopsAndRangesFile.kt
+++ src/main/kotlin/com/kotlinplayground/basics/LoopsAndRangesFile.kt
@@ -6,4 +6,16 @@ fun main() {
     for(i in range){
         println("i : $i")
     }
+
+    val reverseRange = 10 downTo 1
+    for(i in reverseRange){
+        println("i : $i")
+    }
+
+    //it goes 2 by 2
+    for(i in reverseRange step 2){
+        println("ReverseRange with Skip : $i")
+    }
+
+
 }
