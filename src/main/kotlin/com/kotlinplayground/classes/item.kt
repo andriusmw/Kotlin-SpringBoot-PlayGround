53a7f4b8ab1b7ddc61f01b50e55e1ee27efe0d3a
Andres Garcia Larrosa <andriusmw@yahoo.es>
2024-01-29 11:41:28 +0100

    getters and setters

--- src/main/kotlin/com/kotlinplayground/classes/item.kt
+++ src/main/kotlin/com/kotlinplayground/classes/item.kt
@@ -1,7 +1,30 @@
 package com.kotlinplayground.classes
 
+import java.lang.IllegalArgumentException
+
 class Item() {
     var name: String = ""
+    var price: Double = 0.0
+  /*  get() = field
+    //feil gets the las var as reference
+
+   */
+
+    get() {
+        println("Inside getter")
+        return field
+    }
+
+    set(value) {
+        println("Inside thr Setter")
+        if(value>= 0.0) {
+            println("Inside thr Setter > if")
+            field = value
+        } else {
+            throw IllegalArgumentException("Negative price is not allowed")
+        }
+    }
+
 
     constructor(_name: String) : this(){
         name = _name
@@ -13,5 +36,8 @@ fun main() {
     println("Item name is ${item.name}")
     item.name = "Iphone 13"
     println("Item name is ${item.name}")
+
+    item.price = 10.0
+    println(item.price)
 
 }
