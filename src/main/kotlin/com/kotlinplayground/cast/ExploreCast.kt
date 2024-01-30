b5bb4cdef731a2b5b29102e812406d107b60976e
Andres Garcia Larrosa <andriusmw@yahoo.es>
2024-01-30 09:42:53 +0100

    operator AS

--- src/main/kotlin/com/kotlinplayground/cast/ExploreCast.kt
+++ src/main/kotlin/com/kotlinplayground/cast/ExploreCast.kt
@@ -8,9 +8,21 @@ fun checkType(type: Any) {
         is Course -> {
             println(type)
         }
+        is String -> {
+            println(type.lowercase())
+        }
+
+
     }
 }
 
+fun castNumber(any: Any) {
+    when(any){
+        //this only happens if the any we receive is Double Type
+        any as Double -> println("Value is Double")
+        // if not, we get a java exception
+    }
+}
 
 //--------------------------------MAIN-----------------------------------------
 fun main() {
@@ -21,6 +33,18 @@ fun main() {
         "Dilip")
 
     checkType(course)
+    checkType("Dilip")
+
+    castNumber(1.0)
+    //castNumber(1) --> this throws an error cause of the type
+
+    val number = 1
+    val numberDouble = number.toDouble()
+    println(numberDouble)
+    //with this we could convert an Int into a Double for making work our "as Double" Scenario
 }
 
+
+
 // ----------------------------END OF MAIN -------------------------------------------
+
