7126a1cf8fc43a575022fcf6fbd9a050da43f93d
Andres Garcia Larrosa <andriusmw@yahoo.es>
2024-01-30 16:07:58 +0100

    Higher order functions

--- src/main/kotlin/com/kotlinplayground/collections/ExploreLambdas.kt
+++ src/main/kotlin/com/kotlinplayground/collections/ExploreLambdas.kt
@@ -1,5 +1,18 @@
 package com.kotlinplayground.collections
 
+fun calculate(x: Int, y: Int, op: (x:Int,y: Int)-> Int) : Int {
+   return op(x,y)
+    //the third parameter is a lambda whic accepts 2 parameters and return an Int
+    // after that, the return call for the "third parameter with X and y SO IT IS CALLING BACK
+    // TO val result and completes the action on the lambda space, which in this case is
+    // a * b ( 2 * 3) so it is = 6. Yisus!!
+}
+
+
+
+
+
+//-------------------------------------------------
 fun main() {
     val addLambda = {x: Int ->x+x}
 
@@ -11,4 +24,7 @@ fun main() {
         x*y} // result is what you do on the last line
     val addMultipleResult =  multipleLambda(2,3)
     println("addMultipleResult: $addMultipleResult")
+
+    val result = calculate(2,3,{a, b -> a*b})
+    println("Result is $result")
 }
